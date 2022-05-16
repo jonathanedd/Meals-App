cbd8318dab9c4140d6a0811bcf169e094f779295
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-05-15 21:56:52 -0500

    added express-rate-limit and jsonwebtoken

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+// express-rate-limit
+const rateLimit = require('express-rate-limit');
+
+// controllers
+const { globalErrorHandler } = require('./controllers/errors.controller');
+
 // routes
 const { userRouter } = require('./routes/user.routes');
 const { restaurantRouter } = require('./routes/restaurant.routes');
@@ -8,8 +14,20 @@ const app = express();
 
 app.use(express.json());
 
+// limit IP request
+const limiter = rateLimit({
+  max: 10000,
+  windowMs: 60 * 60 * 1000, //1hr
+  message: 'Too many petitions',
+});
+
+app.use(limiter);
+
 // endpoints
 app.use('/api/v4/users', userRouter);
 app.use('/api/v4/restaurants', restaurantRouter);
+
+// global error handler
+app.use('*', globalErrorHandler);
 
 module.exports = { app };
